c761f79dc6fb4149b42ed63244b2cec04bff01f2
Dylan Kario <dylan.kario@gmail.com>
2020-10-22 14:59:32 -0700

    Use packaged chromedriver and headless chromium in layer
    This combination also needs to run on Python 3.7, not 3.8

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM nikolaik/python-nodejs:python3.8-nodejs14
+FROM nikolaik/python-nodejs:python3.7-nodejs14
 
 WORKDIR /usr/src/app
 
--- source/screenshot.py
+++ source/screenshot.py
@@ -14,8 +14,23 @@ def take_screenshot_of_website_graph():
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--hide-scrollbars")
     chrome_options.add_argument("--window-size=760,580")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--user-data-dir=/tmp/user-data")
+    chrome_options.add_argument("--enable-logging")
+    chrome_options.add_argument("--log-level=0")
+    chrome_options.add_argument("--v=99")
+    chrome_options.add_argument("--single-process")
+    chrome_options.add_argument("--data-path=/tmp/data-path")
+    chrome_options.add_argument("--ignore-certificate-errors")
+    chrome_options.add_argument("--homedir=/tmp")
+    chrome_options.add_argument("--disk-cache-dir=/tmp/cache-dir")
+    chrome_options.add_argument(
+        "user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
+    )
+    chrome_options.binary_location = os.getenv("HEADLESS_CHROMIUM")
 
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+    driver = webdriver.Chrome(os.getenv("CHROMEDRIVER"), options=chrome_options)
 
     driver.get("https://projectroomkeytracker.com/")
     driver.execute_script("document.querySelector('h2').scrollIntoView();")
